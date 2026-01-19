c334d357aca94714677764c7c2b8d7fce4b23b4f
Marc-Andre Gardner <marc.andre.gardner@gmail.com>
2026-01-19 13:25:42 -0500

    Modification noms d'executables pour le debogage

--- src/daemonFuse/syncAndStartGDB.sh
+++ src/daemonFuse/syncAndStartGDB.sh
@@ -7,8 +7,8 @@ bn=$(basename $1)
 ssh pi@$2 "mkdir -p /home/pi/projects/laboratoire2/pointdemontage"
 
 # Sync executable
-rsync -az $1/build/tp2client pi@$2:/home/pi/projects/laboratoire2/tp2client
+rsync -az $1/build/setr_tp2_daemonfuse pi@$2:/home/pi/projects/laboratoire2/setr_tp2_daemonfuse
 
 # Execute GDB
-ssh pi@$2 "rm -f /home/pi/capture-stdout-fuse; rm -f /home/pi/capture-stderr-fuse; nohup gdbserver :4568 /home/pi/projects/laboratoire2/tp2client -f -s /home/pi/projects/laboratoire2/pointdemontage > /home/pi/capture-stdout-fuse 2> /home/pi/capture-stderr-fuse < /dev/null &"
+ssh pi@$2 "rm -f /home/pi/capture-stdout-fuse; rm -f /home/pi/capture-stderr-fuse; nohup gdbserver :4568 /home/pi/projects/laboratoire2/setr_tp2_daemonfuse -f -s /home/pi/projects/laboratoire2/pointdemontage > /home/pi/capture-stdout-fuse 2> /home/pi/capture-stderr-fuse < /dev/null &"
 sleep 1
--- src/serveurCurl/syncAndStartGDB.sh
+++ src/serveurCurl/syncAndStartGDB.sh
@@ -7,8 +7,8 @@ bn=$(basename $1)
 ssh pi@$2 "mkdir -p /home/pi/projects/laboratoire2/"
 
 # Sync executable
-rsync -az $1/build/tp2serveur pi@$2:/home/pi/projects/laboratoire2/tp2serveur
+rsync -az $1/build/setr_tp2_serveurcurl pi@$2:/home/pi/projects/laboratoire2/setr_tp2_serveurcurl
 
 # Execute GDB
-ssh pi@$2 "rm -f /home/pi/capture-stdout-curl; rm -f /home/pi/capture-stderr-curl; nohup gdbserver :4567 /home/pi/projects/laboratoire2/tp2serveur > /home/pi/capture-stdout-curl 2> /home/pi/capture-stderr-curl < /dev/null &"
+ssh pi@$2 "rm -f /home/pi/capture-stdout-curl; rm -f /home/pi/capture-stderr-curl; nohup gdbserver :4567 /home/pi/projects/laboratoire2/setr_tp2_serveurcurl > /home/pi/capture-stdout-curl 2> /home/pi/capture-stderr-curl < /dev/null &"
 sleep 1
